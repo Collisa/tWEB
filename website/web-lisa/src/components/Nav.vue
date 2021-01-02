2db3a2570335d3d616f9d3822107db1e936f2bb3
Lisa Collignon <68648800+LisaCollignon@users.noreply.github.com>
2021-01-02 21:47:39 +0100

    Started about page website

--- website/web-lisa/src/components/Nav.vue
+++ website/web-lisa/src/components/Nav.vue
@@ -9,7 +9,7 @@
         <img class="h-4" src="../assets/tWEB.png" alt="">
       </div>
 
-      <button @click="isActive = !isActive" v-click-away="onClickAway" class="md:hidden rounded focus:outline-none focus:ring-2 focus:ring-purplepink-400 focus:ring-opacity-40">
+      <button @click="isActive = !isActive" v-click-away="onClickAway" class="lg:hidden rounded focus:outline-none focus:ring-2 focus:ring-purplepink-400 focus:ring-opacity-40">
         <svg v-show="!isActive" class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
         </svg>
@@ -18,9 +18,9 @@
         </svg>
       </button>
 
-      <div class="absolute md:hidden top-16 right-8">
+      <div class="absolute lg:hidden top-16 right-8 z-10">
 
-        <div v-show="isActive" class="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3 items-end md:items-center bg-gray-950 p-4 md:p-0 rounded-xl md:bg-black">
+        <div v-show="isActive" class="flex flex-col lg:flex-row space-y-3 lg:space-y-0 lg:space-x-3 items-end lg:items-center bg-gray-950 p-4 lg:p-0 rounded-xl lg:bg-black">
           <div class="inline-block relative">
             <router-link
                 exact-active-class="bg-purplepink-400 bg-opacity-40 text-pink-300 text-opacity-50"
@@ -70,11 +70,20 @@
             </a>
           </div>
 
+          <div class="inline-block relative">
+            <a href="http://localhost:8000/dashboard" class="">
+              <p class="p-1 rounded-md hover:bg-opacity-20 hover:text-opacity-50 bg-white text-gray-950 font-semibold text-sm"
+                   :class="getCurrentHoverColor">
+                Costumer Area
+              </p>
+            </a>
+          </div>
+
         </div>
       </div>
 
 
-      <div class="hidden md:flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3 items-end md:items-center bg-gray-950 p-4 md:p-0 rounded-xl md:bg-black">
+      <div class="hidden lg:flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3 items-end md:items-center bg-gray-950 p-4 md:p-0 rounded-xl md:bg-black">
         <div class="inline-block relative">
           <router-link
               exact-active-class="bg-purplepink-400 bg-opacity-40 text-pink-300 text-opacity-50"
@@ -121,6 +130,16 @@
             <img class="w-7 p-1 rounded hover:bg-opacity-20"
                    :class="getCurrentHoverColor"
                    src="../assets/GitHub-Mark-Light-32px.png"/>
+          </a>
+        </div>
+
+        <div class="inline-block relative">
+          <a href="http://localhost:8000/dashboard" class="">
+            <p class="p-1 rounded-md hover:bg-opacity-20 hover:text-opacity-50 bg-white font-semibold text-sm text-gray-950"
+               :class="getCurrentHoverColor">
+              Customer Area
+            </p>
+
           </a>
         </div>
 
--- website/web-lisa/src/views/About.vue
+++ website/web-lisa/src/views/About.vue
@@ -1,13 +1,65 @@
 <template>
-  <h1>About</h1>
+
+  <div class="bg-teal-500 bg-opacity-40 text-center">
+    <h2 class="py-8 text-xl font-semibold">Wat heb ik je te bieden...</h2>
+    <div class="flex flex-row mx-16 py-8 items-center space-x-20">
+      <div class="bg-black rounded-2xl px-6">
+        <h2 class="text-2xl font-semibold py-6">Responsive design</h2>
+        <p class="pb-6 px-6">Elke website is bedoeld om weer te geven op zowel een klein en een groot scherm.</p>
+      </div>
+      <div class="w-48">
+        <img src="../assets/responsive.svg" alt="">
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <div>
+      <img class="w-40" src="../assets/project_management.svg" alt="">
+    </div>
+    <div>
+      <div>
+        <h2>Flexibel ontwerp</h2>
+        <p>Als je zelf al een idee hebt over hoe aller er uit moet zien... Dan mag jij alles in de juiste richting leiden. Ik zorg enkel voor de realisatie van jouw project.</p>
+        <br>
+        <p>En is dat niet het geval? Geen probleem, ik help je mee met het zoeken naar jouw stijl!</p>
+      </div>
+    </div>
+  </div>
+  
+  <div>
+    <div>
+      <h2>Back-end development</h2>
+      <p>Heb je iets meer nodig dan een website?
+      Zoek je een webapplicatie die een deel van je werk kan verlichten?
+      Of een applicatie die net dat beetje extra kan?</p>
+      <br>
+      <p>Ook hiervoor kan gezorgd worden. Ik help je met het ontwikkelen van net datgene dat jij nodig hebt!</p>
+    </div>
+    <div>
+      <img class="w-20" src="../assets/analysis.svg" alt="">
+    </div>
+  </div>
+
+  <div>
+    <div>
+      <img class="w-20" src="../assets/idea.svg" alt="">
+    </div>
+    <div>
+      <div>
+        <h2>Ideeën-bus</h2>
+        <p>Kortom, ik ben er klaar voor. Ik hoop jij ook!
+        Laat je ideeën maar komen.</p>
+        <button
+              class="z-10 mt-6 self-end bg-teal-500 bg-opacity-40 py-1 px-2 rounded-lg text-teal-500 text-opacity-50 text-xs sm:text-sm md:text-base">
+            Let's talk!
+          </button>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      activeLink: this.$emit('Portfolio'),
-    }
-  }
-}
+export default {}
 </script>
